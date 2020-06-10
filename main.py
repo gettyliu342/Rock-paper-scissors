7e47af1bec0f38855633e2884a279c0179f01441
gettyliu342 <60534805+gettyliu342@users.noreply.github.com>
2020-06-09 18:06:17 -0700

    Updates to gameplay
    Still need to add way to track win percentage, formatting

--- main.py
+++ main.py
@@ -7,54 +7,87 @@ print("If there is an invalid input, selection will default to Rock")
 
 print()
 def Rounds():
-    rounds = input("Number of rounds: ")
+    try:
+        rounds = int(input("Number of rounds: "))
+    except:
+        rounds = 3
     return rounds
 
 def choice():
     choice = {"1":"Rock", "2":"Paper", "3":"Scissors"}
     return choice
 
-def outcome(outcome, move1, move2):
-    if outcome == "Tie":
-        message = "Tie Game!"
-    elif outcome == "Win":
+def result(outcome, move1, move2):
+    if outcome == "Win":
         message = (move1 + "beats" + move2 + " You Win!")
     else:
         message = (move2 + "beats" + move1 + " You Lose!")
+    return message
+
+def OUTCOME():
+    if move1 == "Rock":
+        if move2 == "Rock":
+            outcome = "Tie"
+            message = "Tie Game!"
+            print(message)
+        elif move2 == "Paper":
+            outcome = "Lose"
+            message = result(outcome, move1, move2)
+            print(message)
+        else:
+            outcome = "Win"
+            message = result(outcome, move1, move2)
+            print(message)
+    elif move1 == "Paper":
+        if move2 == "Paper":
+            outcome = "Tie"
+            message = "Tie Game!"
+            print(message)
+        elif move2 == "Rock":
+            outcome = "Win"
+            message = result(outcome, move1, move2)
+            print(message)
+        else:
+            outcome = "Lose"
+            message = result(outcome, move1, move2)
+            print(message)
+    else:
+        if move2 == "Paper":
+            outcome = "Win"
+            message = result(outcome, move1, move2)
+            print(message)
+        elif move2 == "Rock":
+            outcome = "Lose"
+            message = result(outcome, move1, move2)
+            print(message)
+        else:
+            outcome = "Tie"
+            message = "Tie Game!"
+            print(message)
 
 rounds = Rounds()
 choice = choice()
 
+
 move1 = ""
 move2 = ""
+outcome = ""
+counter = 1
 
-move1 = input("Rock, paper, scissors, shoot!: ")
-try:
-    move1 = choice[move1]
-except:
-    move1 = "Rock"
+while int(rounds) > 0:
+    print("Round", counter)
+    move1 = input("Rock (1), paper (2), scissors(3), shoot!: ")
+    try:
+        move1 = choice[move1]
+    except:
+        move1 = "Rock"
+    move2 = choice[str(random.randint(1,3))]
+    counter += 1
+    rounds -= 1
 
-move2 = choice[str(random.randint(1,3))]
 
-if move1 == "Rock":
-    if move2 == "Rock":
-        outcome = "Tie"
-    if move2 == "Paper":
-        outcome = "Lose"
-    else:
-        outcome = "Win"
-elif move1 == "Paper":
-    if move2 == "Paper":
-        outcome = "Tie"
-    if move2 == "Rock"
-        outcome = "Win"
-    else:
-        outcome = "Lose"
-else:
-    if move2 == "Paper":
-        outcome = "Win"
-    if move2 == "Rock":
-        outcome = "Lose"
-    else:
-        outcome = "Tie"
+    OUTCOME()
+
+
+
 
